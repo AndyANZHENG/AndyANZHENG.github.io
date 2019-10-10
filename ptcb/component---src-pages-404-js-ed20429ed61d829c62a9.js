4b5cd245855ab5f939b1faaf7ef190a3cf7151fc
AndyANZHENG <zheng.a@yahoo.com>
2019-10-10 17:11:11 +0800

    Updates

--- ptcb/component---src-pages-404-js-ed20429ed61d829c62a9.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{283:function(e,t,n){"use strict";n.r(t);var a=n(0),u=n.n(a),l=n(282),o=n(281);t.default=function(){return u.a.createElement(l.a,null,u.a.createElement(o.a,{title:"404: Not found"}),u.a.createElement("h1",null,"NOT FOUND"),u.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
-//# sourceMappingURL=component---src-pages-404-js-ed20429ed61d829c62a9.js.map
